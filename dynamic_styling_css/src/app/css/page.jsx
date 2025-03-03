68cde930c2d93543525b904afa90839950bbb40a
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-04 01:05:42 +0600

    Text color change after button clicking

--- dynamic_styling_css/src/app/css/page.jsx
+++ dynamic_styling_css/src/app/css/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const Page = () => {
+
+    const [color, setColor] = useState('black')
+
+    const handleText = ()=>{
+        setColor(color === 'black' ? 'blue' : "") //logic: if color is black, so do something.
+    }
     return (
         <div>
-            <h1>CSS</h1>
+            <div className='w-[1100px] mx-auto mt-24'>
+                <h1 style={{color: color}} className='text-3xl'>I love my country</h1>
+                <button onClick={handleText} className='bg-green-600 px-4 py-2 rounded-lg cursor-pointer'>click</button>
+            </div>
         </div>
     );
 }
