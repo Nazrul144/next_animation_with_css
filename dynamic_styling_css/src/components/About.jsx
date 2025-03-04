ce847d804829a945f96022e678fb3d2017b20a21
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-05 01:51:51 +0600

    Showing coupon code after clicking on the button and button disabled ok

--- dynamic_styling_css/src/components/About.jsx
+++ dynamic_styling_css/src/components/About.jsx
@@ -3,10 +3,16 @@ import React, { useState } from 'react';
 const About = () => {
 
     const [disableButton, setDisableButton] = useState('') // Disable button state
+
+    //Logic: I have to declare two state to show Coupon, first I have collect the code by input field and store it in a state then when I click on a button the button call a function and in the function I call another state to set in Input filed value then from this state I just show it. Why did'nt I show the value from the first state? because If I would show from the first state that would not be user friendly because when user types this time it would dynamically show with typing. But I wanted when button is clicked. and I did almost same things for disable button. Happy coding.
+
     const [inputValue, setInputValue] = useState('') // Input state
+    const [showCoupon, setShowCoupon] = useState('') //ShowCoupon Sate.
 
     const handleButton = ()=>{
         setDisableButton('not-allowed')
+        setShowCoupon(inputValue)
+        
     }
 
     const handleInputChange = (event)=>{
@@ -19,7 +25,7 @@ const About = () => {
         <div className='w-[1100px] mx-auto '>
              <div className='mt-20'>
                 <h1>Your Coupon Code: BsgkC</h1>
-                <h1 className='text-center font-black'>Received Coupon:{inputValue}</h1>
+                <h1 className='text-center font-black'>Received Coupon:{showCoupon}</h1>
                 <input onChange={handleInputChange} value={inputValue} className='border-[1px] border-pink-700 px-4 py-2 rounded-lg' type="text" placeholder='Write the coupon code' />
                 <button style={{cursor: disableButton}} onClick={handleButton} className='bg-purple-400 rounded-lg px-4 py-2  cursor-pointer'>
                 Submit
