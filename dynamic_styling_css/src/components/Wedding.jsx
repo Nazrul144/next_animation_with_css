bc88728bdf1fd84f6c7ffcde10aced6e2abe52d8
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-05 04:07:43 +0600

    Button desable added

--- dynamic_styling_css/src/components/Wedding.jsx
+++ dynamic_styling_css/src/components/Wedding.jsx
@@ -1,10 +1,15 @@
 'use client'
+import Image from 'next/image';
 import React, { useState } from 'react';
 
 const Wedding = () => {
 
     const [inputValue, setInputValue] = useState()
     const [money, setMoney] = useState()
+    const [showImage, setShowImage] = useState('none');
+    const [disableButton, setDisableButton] = useState('')
+
+
     const handleInput = (event)=>{
         
         setInputValue(event.target.value)
@@ -17,10 +22,12 @@ const Wedding = () => {
         if(isNaN(inputValue)){
             alert('String is not allowed') //Input validation check number or string.
             setInputValue('')
+            setDisableButton('not-allowed')
         }
         else if(inputValue >= 500){
            setMoney(inputValue)
            setInputValue('')
+           setShowImage((prev)=> prev === 'none'? 'block' : 'none')
         
         }else{
             alert('Pay at least 500 taka!');
@@ -30,11 +37,16 @@ const Wedding = () => {
     }
     return (
         <div className='w-[1100px] mx-auto mt-10'>
+            <div className='flex gap-10'>
             <div>
-                <h1 className='font-bold'>Pay: 500 Taka</h1>
-                <h1 className='font-bold'>You have paid: {money}</h1>
+                <h1 className='font-bold'>Pay: 500 Taka For KFC Chicken</h1>
                 <input onChange={handleInput} value={inputValue} className='px-4 py-2 rounded-lg border-[1px] border-amber-300' type="text" placeholder='Pay your bill' />
-                <button onClick={handleMoney} className='px-4 py-2 rounded-lg bg-green-600 text-white cursor-pointer'>Submit</button>
+                <button style={{cursor: disableButton}} onClick={handleMoney} className='px-4 py-2 rounded-lg bg-green-600 text-white cursor-pointer'>Submit</button>
+            </div>
+            <h1 className='font-bold'>You have paid: {money}</h1>
+            <div>
+                <Image style={{display: showImage}} src='/kfc.webp' alt='KFC_Image' width={400} height={300} />
+            </div>
             </div>
         </div>
     );
